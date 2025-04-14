0dc4e43c97455103f95029ac78c91b9774c6dca5
Sinclair Nzenwata <83012040+starksinclair@users.noreply.github.com>
2025-04-14 11:31:06 -0400

    fixed bugs

--- src/context/PortfolioContext.tsx
+++ src/context/PortfolioContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../utils/firebase";
+// import { collection, getDocs } from "firebase/firestore";
+// import { db } from "../utils/firebase";
 
 type Work = {
   id: string;
