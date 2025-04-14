b8f2af9cbea151d447df6bce550ce6d52d2f5695
Sinclair Nzenwata <83012040+starksinclair@users.noreply.github.com>
2025-04-14 11:43:09 -0400

    fixed the function, removed client code and used serveless code for firebase

--- api/function.ts
+++ api/function.ts
@@ -1,23 +1,22 @@
 import { VercelResponse } from "@vercel/node";
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getFirestore();
 
 export default async function handler(res: VercelResponse) {
   try {
-    const querySnapshot = await getDocs(collection(db, "portfolio"));
+    const querySnapshot = await db.collection("portfolio").get();
     const data = querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
