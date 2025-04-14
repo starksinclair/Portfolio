5a9dfb9f755572be4f13ba92b32af26c85cbf8b7
Sinclair Nzenwata <83012040+starksinclair@users.noreply.github.com>
2025-04-14 12:34:52 -0400

    bruteforce

--- api/function.ts
+++ api/function.ts
@@ -1,6 +1,18 @@
 import { VercelResponse } from "@vercel/node";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+// };
 
 if (!getApps().length) {
   initializeApp({
@@ -11,18 +23,25 @@ if (!getApps().length) {
     }),
   });
 }
-
 const db = getFirestore();
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
 export default async function handler(res: VercelResponse) {
   try {
-    // const querySnapshot = await db.collection("portfolio").get();
+    // const querySnapshot = await getDocs(collection(db, "portfolio"));
     // const data = querySnapshot.docs.map((doc) => ({
     //   id: doc.id,
     //   ...doc.data(),
     // }));
-    console.log("Fetched data from Firebase:");
-    res.status(200).send("Fetched data from Firebase successfully");
+    const snapshot = await db.collection("portfolio").get();
+    const data = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    console.log("Fetched data from Firebase:", data);
+    res.send(data);
   } catch (error) {
     console.error("Error fetching data from Firebase:", error);
     res.status(500).json({ error: "Failed to fetch data" });
